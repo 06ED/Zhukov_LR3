ec72d2aff6d53eff09c3a2f375551203aa3e47d6
06ED <hsbest14@yandex.ru>
2025-03-14 11:22:49 +0300

    Create sturcture

--- Zhukov_LR3_project.cpp
+++ Zhukov_LR3_project.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <map>
+#include <functional>
 
 using namespace std;
 
+class UnimplementedExeption: public exception
+{
+  virtual const char* what() const throw()
+  {
+    return "This method are not implemented";
+  }
+} unimplemented;
+
+void enter_a() {
+    throw unimplemented; // TODO implement me
+}
+
+void enter_b() {
+    throw unimplemented; // TODO implement me
+}
+
+void get_reminder() {
+    throw unimplemented; // TODO implement me
+}
+
+void get_result() {
+    throw unimplemented; // TODO implement me
+}
+
+struct MenuItem {
+    function<void()> callback;
+    string title;
+
+    MenuItem() = default;
+
+    MenuItem(string title, function<void()> callback)
+        : title(move(title)), callback(move(callback)) {
+    }
+};
+
 int main() {
-    cout << "";
+    map<int, MenuItem> menu{
+        {1, MenuItem("Ввести A: ", enter_a)},
+        {2, MenuItem("Ввести B", enter_b)},
+        {3, MenuItem("Получить остаток от деления a на b", get_reminder)},
+        {4, MenuItem("Получить результат от деления b на а", get_result)},
+    };
+
 
     return 0;
 }
